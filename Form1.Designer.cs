cefe9ced13093d6a407e61d65c2ebd86b83f8177
saha209 <wagashi.209@gmail.com>
2020-11-16 18:44:04 +0900

    完成品

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace checkUSB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "接続";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "切断";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "type2keyboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,550 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+using System.Threading;
+
+namespace checkUSB
+{
+
+    public partial class Form1 : Form
+    {
+
+        public static UsbDevice MyUsbDevice;
+        public static UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(0x0AE4, 0x0101);
+        bool IsDisposing = false;//Disposeが実行中かどうかをチェック
+        private string senddata = string.Empty;
+        private string Pnotchsend = string.Empty;
+        private string Bnotchsend = string.Empty;
+        private string beforsend = string.Empty;
+        private string senddata1 = string.Empty;
+        private string senddata2 = string.Empty;
+        private string senddata3 = string.Empty;
+        private string senddata4 = string.Empty;
+        private string senddata5 = string.Empty;
+        private string senddata6 = string.Empty;
+        private string senddata7 = string.Empty;
+        private string senddata8 = string.Empty;
+        private string senddata9 = string.Empty;
+        private string senddata10 = string.Empty;
+        private string senddata11 = string.Empty;
+        int nownotch = 0;
+        int nowbrake = 0;
+        int bottonnum1befor = 0;
+        int bottonnum10befor = 0;
+        int bottonnum100befor = 0;
+        int bottonnum1000befor = 0;
+        int bottonnum10000befor = 0;
+        int bottonnum100000befor = 0;
+        int bottonnum1_befor = 0;
+        int bottonnum10_befor = 0;
+        int bottonnum100_befor = 0;
+        int bottonnum1000_befor = 0;
+        int bottonnum10000_befor = 0;
+        int bottonnum100000_befor = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "切断中";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ErrorCode ec = ErrorCode.None;
+
+            try
+            {
+                MyUsbDevice = UsbDevice.OpenUsbDevice(MyUsbFinder);
+
+                if (MyUsbDevice == null) throw new Exception("Device Not Found.");
+                IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                if (!ReferenceEquals(wholeUsbDevice, null))
+                {
+                    IsDisposing = false;
+                    wholeUsbDevice.SetConfiguration(1);
+                    wholeUsbDevice.ClaimInterface(0);
+                    label1.Text = "接続完了";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ec != ErrorCode.None ? ec + ":" : String.Empty + ex.Message;
+            }
+
+            Thread thread = new Thread(new ThreadStart(() => {
+                while (!IsDisposing)
+                {//Disposeが呼ばれるまで無限ループ
+                    DataRead();
+                }
+            }));
+            thread.Start();
+
+            Thread thread2 = new Thread(new ThreadStart(() => {
+                while (!IsDisposing)
+                {//Disposeが呼ばれるまで無限ループ
+                    Senddata();
+                }
+            }));
+            thread2.Start();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MyUsbDevice = null;
+            IsDisposing = true;
+            label1.Text = "切断中";
+        }
+        
+
+
+        private void DataRead()
+        {
+            ErrorCode ec = ErrorCode.None;
+            UsbEndpointReader reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01);
+            byte[] readBuffer = new byte[8];
+            int bytesRead;
+            ec = reader.Read(readBuffer, 100, out bytesRead);
+            String str = BitConverter.ToString(readBuffer);
+            //label2.Text = str;
+
+            switch (readBuffer[1])
+            {
+                case 0x01:
+                    //label3.Text = "EB";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x02:
+                    //label3.Text = "B7";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x03:
+                    //label3.Text = "B6";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x04:
+                    //label3.Text = "B5";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x05:
+                    //label3.Text = "B4";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x06:
+                    //label3.Text = "B3";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x07:
+                    //label3.Text = "B2";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x08:
+                    //label3.Text = "B1";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x09:
+                    //label3.Text = "N";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x0A:
+                    //label3.Text = "p1";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x0B:
+                    //label3.Text = "p2";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x0C:
+                    //label3.Text = "p3";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x0D:
+                    //label3.Text = "p4";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x41:
+                    //label3.Text = "EB";
+                    nownotch = 0;
+                    nowbrake = 9;
+                    break;
+                case 0x42:
+                    //label3.Text = "B7";
+                    nownotch = 0;
+                    nowbrake = 7;
+                    break;
+                case 0x43:
+                    //label3.Text = "B6";
+                    nownotch = 0;
+                    nowbrake = 6;
+                    break;
+                case 0x44:
+                    //label3.Text = "B5";
+                    nownotch = 0;
+                    nowbrake = 5;
+                    break;
+                case 0x45:
+                    //label3.Text = "B4";
+                    nownotch = 0;
+                    nowbrake = 4;
+                    break;
+                case 0x46:
+                    //label3.Text = "B3";
+                    nownotch = 0;
+                    nowbrake = 3;
+                    break;
+                case 0x47:
+                    //label3.Text = "B2";
+                    nownotch = 0;
+                    nowbrake = 2;
+                    break;
+                case 0x48:
+                    //label3.Text = "B1";
+                    nownotch = 0;
+                    nowbrake = 1;
+                    break;
+                case 0x49:
+                    //label3.Text = "N";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x4A:
+                    //label3.Text = "p1";
+                    nownotch = 1;
+                    nowbrake = 0;
+                    break;
+                case 0x4B:
+                    //label3.Text = "p2";
+                    nownotch = 2;
+                    nowbrake = 0;
+                    break;
+                case 0x4C:
+                    //label3.Text = "p3";
+                    nownotch = 3;
+                    nowbrake = 0;
+                    break;
+                case 0x4D:
+                    //label3.Text = "p4";
+                    nownotch = 4;
+                    nowbrake = 0;
+                    break;
+                case 0x81:
+                    //label3.Text = "EB";
+                    nownotch = 0;
+                    nowbrake = 9;
+                    break;
+                case 0x82:
+                    //label3.Text = "B7";
+                    nownotch = 0;
+                    nowbrake = 7;
+                    break;
+                case 0x83:
+                    //label3.Text = "B6";
+                    nownotch = 0;
+                    nowbrake = 6;
+                    break;
+                case 0x84:
+                    //label3.Text = "B5";
+                    nownotch = 0;
+                    nowbrake = 5;
+                    break;
+                case 0x85:
+                    //label3.Text = "B4";
+                    nownotch = 0;
+                    nowbrake = 4;
+                    break;
+                case 0x86:
+                    //label3.Text = "B3";
+                    nownotch = 0;
+                    nowbrake = 3;
+                    break;
+                case 0x87:
+                    //label3.Text = "B2";
+                    nownotch = 0;
+                    nowbrake = 2;
+                    break;
+                case 0x88:
+                    //label3.Text = "B1";
+                    nownotch = 0;
+                    nowbrake = 1;
+                    break;
+                case 0x89:
+                    //label3.Text = "N";
+                    nownotch = 0;
+                    nowbrake = 0;
+                    break;
+                case 0x8A:
+                    //label3.Text = "p1";
+                    nownotch = 1;
+                    nowbrake = 0;
+                    break;
+                case 0x8B:
+                    //label3.Text = "p2";
+                    nownotch = 2;
+                    nowbrake = 0;
+                    break;
+                case 0x8C:
+                    //label3.Text = "p3";
+                    nownotch = 3;
+                    nowbrake = 0;
+                    break;
+                case 0x8D:
+                    //label3.Text = "p4";
+                    nownotch = 4;
+                    nowbrake = 0;
+                    break;
+            }
+
+            
+
+            
+
+            if (nownotch == 0)
+            {
+                Pnotchsend = null;
+            }
+            else if (nownotch > 0)
+            {
+                Pnotchsend = "A";
+            }
+            if (nowbrake == 0)
+            {
+                Bnotchsend = null;
+            }
+            else if (nowbrake > 0 && nowbrake != 9)
+            {
+                Bnotchsend = "S";
+            }
+            else if (nowbrake == 9)
+            {
+                Bnotchsend = "D";
+            }
+            
+
+            //ボタンの処理
+            string bottonbin_ = Convert.ToString(readBuffer[3], 2);
+            int bottonnum_ = Convert.ToInt32(bottonbin_);
+
+            //start・未実装
+            int bottonnum1_ = bottonnum_ - (bottonnum_ / 10) * 10;
+            if (bottonnum1_ != bottonnum1_befor)
+            {
+                if (bottonnum1_ % 2 == 1)
+                {
+                    senddata1 = null;
+                }
+                else
+                {
+                    senddata1 = null;
+                }
+                bottonnum1_befor = bottonnum1_;
+            }
+
+            //select・"esc"
+            int bottonnum10_ = (bottonnum_ - (bottonnum_ / 100) * 100) / 10;
+            if (bottonnum10_ != bottonnum10_befor)
+            {
+                if (bottonnum10_ % 2 == 1)
+                {
+                    senddata2 = "{esc}";
+                }
+                else
+                {
+                    senddata2 = null;
+                }
+                bottonnum10_befor = bottonnum10_;
+            }
+
+            //↑
+            int bottonnum100_ = (bottonnum_ - (bottonnum_ / 1000) * 1000) / 100;
+            if (bottonnum100_ != bottonnum100_befor)
+            {
+                if (bottonnum100_ % 2 == 1)
+                {
+                    senddata3 = "{UP}";
+                }
+                else
+                {
+                    senddata3 = null;
+                }
+                bottonnum100_befor = bottonnum100_;
+            }
+
+            //↓
+            int bottonnum1000_ = (bottonnum_ - (bottonnum_ / 10000) * 10000) / 1000;
+            if (bottonnum1000_ != bottonnum1000_befor)
+            {
+                if (bottonnum1000_ % 2 == 1)
+                {
+                    senddata4 = "{DOWN}";
+                }
+                else
+                {
+                    senddata4 = null;
+                }
+                bottonnum1000_befor = bottonnum1000_;
+            }
+
+
+            //←
+            int bottonnum10000_ = (bottonnum_ - (bottonnum_ / 100000) * 100000) / 10000;
+            if (bottonnum10000_ != bottonnum10000_befor)
+            {
+                if (bottonnum10000_ % 2 == 1)
+                {
+                    senddata5 = "{LEFT}";
+                }
+                else
+                {
+                    senddata5 = null;
+                }
+                bottonnum10000_befor = bottonnum10000_;
+            }
+
+            //→
+            int bottonnum100000_ = (bottonnum_ - (bottonnum_ / 1000000) * 1000000) / 100000;
+            if (bottonnum100000_ != bottonnum100000_befor)
+            {
+                if (bottonnum100000_ % 2 == 1)
+                {
+                    senddata6 = "{RIGHT}";
+                }
+                else
+                {
+                    senddata6 = null;
+                }
+                bottonnum100000_befor = bottonnum100000_;
+            }
+
+
+            //ボタンの処理
+            string bottonbin = Convert.ToString(readBuffer[2], 2);
+                int bottonnum = Convert.ToInt32(bottonbin);
+
+            //S・未実装
+            int bottonnum1 = bottonnum - (bottonnum / 10) * 10;
+            if (bottonnum1 != bottonnum1befor)
+            {
+                if (bottonnum1 % 2 == 1)
+                {
+                    senddata7 = null;
+                }
+                else
+                {
+                    senddata7 = null;
+                }
+                bottonnum1befor = bottonnum1;
+            }
+
+            //D・"W"
+            int bottonnum10 = (bottonnum - (bottonnum / 100) * 100) / 10;
+            if (bottonnum10 != bottonnum10befor)
+            {
+                if (bottonnum10 % 2 == 1)
+                {
+                    senddata8 = "W";
+                }
+                else
+                {
+                    senddata8 = null;
+                }
+                bottonnum10befor = bottonnum10;
+            }
+
+            //A・"Q"
+            int bottonnum100 = (bottonnum - (bottonnum / 1000) * 1000) / 100;
+            if (bottonnum100 != bottonnum100befor)
+            {
+                if (bottonnum100 % 2 == 1)
+                {
+                    senddata9 = "Q";
+                }
+                else
+                {
+                    senddata9 = null;
+                }
+                bottonnum100befor = bottonnum100;
+            }
+
+            //A深押し
+            int bottonnum1000 = (bottonnum - (bottonnum / 10000) * 10000) / 1000;
+            if (bottonnum1000 != bottonnum1000befor)
+            {
+                bottonnum1000befor = bottonnum1000;
+            }
+
+
+            //B・"エンター"
+            int bottonnum10000 = (bottonnum - (bottonnum / 100000) * 100000) / 10000;
+            if (bottonnum10000 != bottonnum10000befor)
+            {
+                if (bottonnum10000 % 2 == 1)
+                {
+                    senddata10 = "{ENTER}";
+                }
+                else
+                {
+                    senddata10 = null;
+                }
+                bottonnum10000befor = bottonnum10000;
+            }
+
+            //C・未実装
+            int bottonnum100000 = (bottonnum - (bottonnum / 1000000) * 1000000) / 100000;
+            if (bottonnum100000 != bottonnum100000befor)
+            {
+                if (bottonnum100000 % 2 == 1)
+                {
+                    senddata11 = null;
+                }
+                else
+                {
+                    senddata11 = null;
+                }
+                bottonnum100000befor = bottonnum100000;
+            }
+
+
+
+
+
+            senddata = senddata1 + senddata2 + senddata3 + senddata4 + senddata5 + senddata6 + senddata7+ senddata8 + senddata9 + senddata10 + senddata11 + Pnotchsend + Bnotchsend;
+
+            beforsend = senddata;
+        }
+
+
+        private void Senddata()
+        {
+            SendKeys.SendWait(senddata);
+        }
+
+    }
+
+}
